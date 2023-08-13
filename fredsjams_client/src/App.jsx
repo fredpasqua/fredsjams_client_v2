6ad385d8444f984990f705bfe87ce995b8524500
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2023-08-13 13:55:34 -0400

    first commit

---
+++ fredsjams_client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import MainPage  from './Components/mainSelectors/mainSelector.jsx'
+function App() {
+
+
+  return (
+    <>
+     
+      <MainPage/>
+
+     
+    </>
+  )
+}
+
+export default App
