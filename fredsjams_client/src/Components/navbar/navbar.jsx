757fe6ebaf6fb1bb7125ea02a1b4bba36e9d12b1
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2023-08-14 17:01:45 -0400

    fixed navbar

--- fredsjams_client/src/Components/navbar/navbar.jsx
+++ fredsjams_client/src/Components/navbar/navbar.jsx
@@ -1,43 +1,47 @@
-import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+
 import { Link } from "react-router-dom";
 import fredsjams from "../../assets/fredsjams.png";
 import "./navbar.css";
 function Navigation() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand>
-          <Link to="/fredsjams_client_v2/" className="logo">
-            <img src={fredsjams} alt="logo"></img>
-          </Link>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link>
-              <Link to="/Gear-Reviews">Gear Reviews</Link>
+    <Navbar collapseOnSelect data-bs-theme="dark" expand="lg">
+      <Navbar.Brand>
+        <Link to="/fredsjams_client_v2/" className="logo">
+          <img src={fredsjams} alt="logo"></img>
+        </Link>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse collapseOnSelect id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Item>
+            <Nav.Link eventKey="1" as={Link} to="/Gear-Reviews">
+              Gear Reviews
             </Nav.Link>
-
-            <Nav.Link>
-              <Link to="/Music-Reviews">Music Reviews</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="2" as={Link} to="/Music-Reviews">
+              Music Reviews
             </Nav.Link>
-
-            <Nav.Link>
-              <Link to="/Notes">Notes</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="3" as={Link} to="/Notes">
+              Notes
             </Nav.Link>
-            <Nav.Link>
-              {" "}
-              <Link to="/News">News</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="4" as={Link} to="/News">
+              News
             </Nav.Link>
-            <Nav.Link>
-              {" "}
-              <Link to="/About">About</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="5" as={Link} to="/About">
+              About
             </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
