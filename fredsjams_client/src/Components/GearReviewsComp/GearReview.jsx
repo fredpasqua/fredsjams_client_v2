40356e2284280f6175de2d132637ef4ae38017c1
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2023-08-26 21:20:23 -0400

    updates to build gear reviews page

---
+++ fredsjams_client/src/Components/GearReviewsComp/GearReview.jsx
@@ -0,0 +1,46 @@
+import "./MusicReview.css";
+function MusicReview(props) {
+  const Name = props.title;
+  const shortDescription = props.description;
+  const image = props.image;
+  const imageAlt = props.imageAlt;
+  const releasedOn = props.releasedOn;
+  const Artist = props.artist;
+  const Rating = props.rating;
+  const releaseYear = props.releaseYear;
+  const genre = props.genre;
+  const style = props.style;
+  const format = props.format;
+  console.log(image);
+  return (
+    <div>
+      <div className="reviewContainer">
+        <h1 className="title">{Name}</h1>
+        <div className="image-container">
+          {" "}
+          <img className="image" src={image} alt={imageAlt}></img>
+          <div className="item-info-container">
+            <ul className="info-list">
+              <h2>{Artist}</h2>
+              <li>Release Year: {releaseYear}</li>
+              <li>Label: {releasedOn}</li>
+              <li>Rating: {Rating} </li>
+              <li>Genre: {genre}</li>
+              <li>Style: {style}</li>
+              <li>Format: {format}</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="title-description-container">
+          <hr></hr>
+          <h3 className="date">Posted: June 1, 2023</h3>
+          <h3 className="date">Reviewer: Staff Reviewer</h3>
+          <p> {shortDescription}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MusicReview;
--- fredsjams_client/src/Pages/MusicReviews/MusicReviews.jsx
+++ fredsjams_client/src/Pages/MusicReviews/MusicReviews.jsx
@@ -27,54 +27,6 @@ function Reviews() {
             image={item.ImageUrl}
           />
         ))}
-        <MusicReview
-          title={"Abraxas"}
-          description={`
-            The swirling atmospherics of the opening Singing Winds Crying Beats merge seamlessly into the languid Black Magic Woman the combustible Gypsy Queen and the vibrant Oye Como Va to produce a continuous 15-minute suite that portrays the band’s broad emotional range. 
-
-            There are also the complex, jazz-influenced Incident At Neshabur, more percussion pyrotechnics on Se A Cabo, and the sublime instrumental Samba Pa Ti, which has become Carlos’s signature tune.
-            
-            "I felt Santana had a very dynamic range," Metallica's Rob Trujillo told us. "He’d bring in heavy guitars for the riff to get everyone excited, and with the use of percussion there was this indigenous quality to the beat, the rhythm, the flow of the music he was producing - not to mention all those nice, beautiful instrumental moments.
-            
-            “The album cover was really fascinating too. His artwork was psychedelic and trippy with beautiful women in it that made you look and go ‘wow’. There were many emotions stirred with that record."`}
-          image={"https://m.media-amazon.com/images/I/91eRdbn09dL._SL1500_.jpg"}
-          imageAlt={"an image of this album cover"}
-          artist={"Santana"}
-          releasedOn={"Polygram Records"}
-          releaseYear={"1969"}
-          rating={"Four Stars!"}
-          ss
-        />
-        <MusicReview
-          title={"Revolver"}
-          description={
-            "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsumm"
-          }
-          image={
-            "https://m.media-amazon.com/images/I/61KgIpfj1UL._AC_UY218_.jpg"
-          }
-          imageAlt={"an image of this album Revolver cover"}
-          artist={"The Beatles"}
-          releasedOn={"Polygram Records"}
-          releaseYear={"1969"}
-          rating={"Four Stars!"}
-          ss
-        />
-        <MusicReview
-          title={"1989"}
-          description={
-            "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsumm"
-          }
-          artist={"Taylor Swift"}
-          image={
-            "https://m.media-amazon.com/images/I/61wablwq9HL._AC_UY218_.jpg"
-          }
-          imageAlt={"an image of this album cover"}
-          releasedOn={"Polygram Records"}
-          releaseYear={"1969"}
-          rating={"Four Stars!"}
-          ss
-        />
       </div>
     </>
   );
