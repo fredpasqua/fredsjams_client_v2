701e65f79d0280028d7be3275dd6f57c2fe296ee
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2023-08-26 21:04:17 -0400

    add state to handle expanded state of nav

--- fredsjams_client/src/Components/navbar/navbar.jsx
+++ fredsjams_client/src/Components/navbar/navbar.jsx
@@ -3,45 +3,76 @@ import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import fredsjams from "../../assets/fredsjams.png";
 import "./navbar.css";
+import { useState } from "react";
 function Navigation() {
+  const [expanded, setExpanded] = useState(false);
   return (
     <Navbar
       collapseOnSelect
       data-bs-theme="dark"
+      expanded={expanded}
       expand="lg"
       sticky="top"
       className="navOpacity"
     >
       <Navbar.Brand>
-        <Link to="/fredsjams_client_v2/">
+        <Link to="/fredsjams_client_v2/" onClick={() => setExpanded(false)}>
           <img className="logo" src={fredsjams} alt="logo"></img>
         </Link>
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Toggle
+        aria-controls="basic-navbar-nav"
+        onClick={() => setExpanded(!expanded)}
+      />
       <Navbar.Collapse collapseOnSelect id="basic-navbar-nav">
         <Nav className="me-auto">
           <Nav.Item>
-            <Nav.Link eventKey="1" as={Link} to="/Gear-Reviews">
+            <Nav.Link
+              eventKey="1"
+              as={Link}
+              to="/Gear-Reviews"
+              onClick={() => setExpanded(!expanded)}
+            >
               Gear Reviews
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="2" as={Link} to="/Music-Reviews">
+            <Nav.Link
+              eventKey="2"
+              as={Link}
+              to="/Music-Reviews"
+              onClick={() => setExpanded(!expanded)}
+            >
               Music Reviews
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="3" as={Link} to="/Notes">
+            <Nav.Link
+              eventKey="3"
+              as={Link}
+              to="/Notes"
+              onClick={() => setExpanded(!expanded)}
+            >
               Notes
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="4" as={Link} to="/News">
+            <Nav.Link
+              eventKey="4"
+              as={Link}
+              to="/News"
+              onClick={() => setExpanded(!expanded)}
+            >
               News
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="5" as={Link} to="/About">
+            <Nav.Link
+              eventKey="5"
+              as={Link}
+              to="/About"
+              onClick={() => setExpanded(!expanded)}
+            >
               About
             </Nav.Link>
           </Nav.Item>
