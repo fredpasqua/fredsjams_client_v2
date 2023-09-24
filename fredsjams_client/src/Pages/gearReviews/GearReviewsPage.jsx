de3752ad2986be40523539aa59b51207d3266af9
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2023-09-24 09:38:46 -0400

    add search bar to gear review component

--- fredsjams_client/src/Pages/gearReviews/GearReviewsPage.jsx
+++ fredsjams_client/src/Pages/gearReviews/GearReviewsPage.jsx
@@ -1,16 +1,42 @@
 import "./GearReviewsPage.css";
 import GearReview from "../../Components/GearReviewsComp/GearReview.jsx";
 import data from "../../assets/gearData.json";
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useState } from "react";
 function GearReviews() {
   useLayoutEffect(() => {
     window.scrollTo(0, 0);
   });
+  const [searchInput, setSearchInput] = useState("");
+  const [articles] = useState(data);
+  const [filteredArticles, setFilteredArticles] = useState([]);
+
+  const handleSearchInput = (event) => {
+    const input = event.target.value;
+    setSearchInput(input);
+
+    const filtered = articles.filter((article) => {
+      const searchText = `${article.Brand} ${article.Type} ${article.Model} ${article.Features} ${article.DriveMethod} ${article.Speeds}`;
+      return searchText.toLowerCase().includes(input.toLowerCase());
+    });
+
+    setFilteredArticles(filtered);
+  };
+
   return (
     <>
       <div className="gearReview">
         <h1>Turntables, Speakers, and More...</h1>
-        {data.map((item) => (
+        <div>
+          <p>Filter Results</p>
+          <input
+            type="text"
+            placeholder="Search articles"
+            value={searchInput}
+            onChange={handleSearchInput}
+          />
+        </div>
+
+        {filteredArticles.map((item) => (
           <GearReview
             key={item.Model}
             title={item.Brand + " " + item.Model}
